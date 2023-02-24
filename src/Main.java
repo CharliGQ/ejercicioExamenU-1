acc9e31ac7e4267cee169c46a01b7c00548e880f
charsthg <l22390451@chetumal.tecnm.mx>
2023-02-24 12:06:07 -0500

    Agregue el PDF

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entrada=new Scanner(System.in);
+
+        double n1, n2, div=0, res, res1;
+
+        System.out.println("Ingrese dos numeros:");
+        n1=entrada.nextDouble();
+        n2=entrada.nextDouble();
+
+        div=n1-n2;
+
+        res=n1/div;
+        res1=n2/div;
+
+        System.out.println(res);
+        System.out.println(res1);
+
+
     }
 }
